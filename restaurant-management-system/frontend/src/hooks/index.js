e7f211e0315c9c3e3cf1cbfd4ee2b15fbfdf5989
Gyanendra Shah <shahgyanendra555@gmail.com>
2026-03-10 14:33:54 +0530

    change in frontend

---
+++ restaurant-management-system/frontend/src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default } from './useOrderTracking.js';
--- restaurant-management-system/frontend/vite.config.js
+++ restaurant-management-system/frontend/vite.config.js
@@ -1,8 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@services': path.resolve(__dirname, './src/services')
+        }
+    },
     server: {
         port: 3000,
         open: true,
