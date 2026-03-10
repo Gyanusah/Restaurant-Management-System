675f25f9ae3ae93da4f2bc3cb0d40cf1cdaa1b67
Gyanendra Shah <shahgyanendra555@gmail.com>
2026-03-10 21:45:38 +0530

    change in socket

--- restaurant-management-system/backend/socket.js
+++ restaurant-management-system/backend/socket.js
@@ -7,7 +7,7 @@ export const init = (server) => {
     cors: {
       origin: [
         'http://localhost:3000',
-        'http://localhost:5173',
+        'https://restaurant-management-system-gilt.vercel.app',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173'
       ],
