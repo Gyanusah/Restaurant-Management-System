57a439a8968eddf4bd38c2b0abc4679a31d0256a
Gyanendra Shah <shahgyanendra555@gmail.com>
2026-03-10 16:25:44 +0530

    change in vite.config

--- restaurant-management-system/frontend/vite.config.js
+++ restaurant-management-system/frontend/vite.config.js
@@ -1,3 +1,34 @@
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import path from 'path'
+
+// export default defineConfig({
+//     plugins: [react()],
+//     resolve: {
+//         alias: {
+//             '@': path.resolve(__dirname, './src'),
+//             '@hooks': path.resolve(__dirname, './src/hooks'),
+//             '@services': path.resolve(__dirname, './src/services')
+//         }
+//     },
+//     server: {
+//         port: 3000,
+//         open: true,
+//     },
+//     build: {
+//         outDir: 'dist',
+//         assetsDir: 'assets',
+//         sourcemap: true,
+//         rollupOptions: {
+//             output: {
+//                 manualChunks: undefined
+//             }
+//         }
+//     },
+//     base: process.env.NODE_ENV === 'production' ? '/restaurant-management-system/' : '/',
+// })
+
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
@@ -13,17 +44,18 @@ export default defineConfig({
     },
     server: {
         port: 3000,
-        open: true,
+        open: true
     },
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
         sourcemap: true,
+        chunkSizeWarningLimit: 1000,
         rollupOptions: {
             output: {
                 manualChunks: undefined
             }
         }
     },
-    base: process.env.NODE_ENV === 'production' ? '/restaurant-management-system/' : '/',
+    base: '/'
 })
